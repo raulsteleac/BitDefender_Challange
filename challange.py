8feb1b1da720f3a1f5581bcaa72d90a8bf5efba0
Steleac Raul <raul.steleac@student.upt.ro>
2019-05-26 22:38:58 +0300

    Add files via upload

--- challange.py
+++ challange.py
@@ -1,4 +1,6 @@
 #%% Change working directory from the workspace root to the ipynb file location.
+import tensorflow as tf
+import numpy as np
 import os
 try:
     os.chdir(os.path.join(
@@ -7,14 +9,14 @@ try:
 except:
     pass
 
-data_file = 'challenge_train.csv'
+data_file_train = 'challenge_train.csv'
+data_file_test  = 'challenge_test.csv'
 from reader import Reader
-import tensorflow as tf 
-import numpy as np
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 class BitDefender_Data_Producer(object):
       def __init__(self, config):
-            self._data_producer = Reader(config.data_file, config.batch_size, config.validation_split, config.test_split, config.variance_threshold)
+            self._data_producer = Reader(config.feature_extraction_method ,config.data_file, config.test_file, config.batch_size, config.validation_split, config.variance_threshold)
 
       def import_data(self, session):
             (self.train_inputs, self.train_targets), (self.validation_inputs, self.validation_targets), (self.test_inputs, self.test_targets), (self.train_batch_nr, self.validation_batch_nr, self.test_batch_nr) = self._data_producer.batch_producer(session)
@@ -34,7 +36,7 @@ class BitDefender_Data_Producer(object):
             self._data_producer.free_threads()
 
 class BitDefender_Challanger(object):
-      def __init__(self, inputs, targets, batch_nr, is_training=False, learning_rate=0, model_op_name=None):
+      def __init__(self, inputs, targets=None, batch_nr=1, is_training=False, learning_rate=0, model_op_name=None):
             self._learning_rate = learning_rate
             self.init = tf.random_uniform_initializer(-0.1, 0.1)
             self.model_op_name = model_op_name
@@ -48,13 +50,24 @@ class BitDefender_Challanger(object):
             session.run(tf.global_variables_initializer())
 
       def model(self):
+            """ CREATE CHALLANGE MODEL WITH 4 HIDDEN LAYERS FOR CLASSIGICATION USING SIGMOID CROSS ENTROPY LOSS
+                cross_entropy = sigmoid cross entropy loss
+                Y_ = predictions
+                is_correct = tensor of 1s and 0s representing the correct and incorrect predictions
+                Class data members:
+                self.accuracy = actual calculated accuracy 
+                self.optimizer = minimization tensor op using Adam Optimizer
+            """
             with tf.variable_scope("BitDefender_Challanger", reuse=tf.AUTO_REUSE, initializer=self.init):
-                  hidden_layer_1 = tf.layers.dense(self.inputs  , 128, activation=tf.nn.relu, name="First_Fully_Connected_Layer" )
-                  hidden_layer_2 = tf.layers.dense(hidden_layer_1, 64, activation=tf.nn.relu, name="Second_Fully_Connected_Layer")
-                  hidden_layer_3 = tf.layers.dense(hidden_layer_2, 32, activation=tf.nn.relu, name="Third_Fully_Connected_Layer" )
-                  hidden_layer_4 = tf.layers.dense(hidden_layer_3, 16, activation=tf.nn.relu, name="Forth_Fully_Connected_Layer" )
+                  hidden_layer_1 = tf.layers.dense(self.inputs, 128, activation=tf.nn.relu, name="First_Fully_Connected_Layer")
+                  hidden_layer_1 = self.batch_normalization(hidden_layer_1)
+                  hidden_layer_2 = tf.layers.dense(hidden_layer_1, 96, activation=tf.nn.relu, name="Second_Fully_Connected_Layer")
+                  hidden_layer_2 = self.batch_normalization(hidden_layer_2)
+                  hidden_layer_3 = tf.layers.dense(hidden_layer_2, 64, activation=tf.nn.relu, name="Third_Fully_Connected_Layer" )
+                  hidden_layer_3 = self.batch_normalization(hidden_layer_3)
+                  hidden_layer_4 = tf.layers.dense(hidden_layer_3, 32, activation=tf.nn.relu, name="Forth_Fully_Connected_Layer" )
                   output_layer   = tf.layers.dense(hidden_layer_4,  1, name="Output_Layer")   
-                                                                        
+
                   cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.targets, logits=output_layer)
                   Y_ = tf.nn.sigmoid(output_layer)
                   is_correct = tf.equal(tf.round(Y_), self.targets)
@@ -67,8 +80,19 @@ class BitDefender_Challanger(object):
                   adam_opt = tf.train.AdamOptimizer(self._learning_rate)
                   self.optimizer = adam_opt.minimize(cross_entropy)
 
+      def batch_normalization(self, batch): 
+            """ COMPUTE BATCH NORMALIZAION ON THE BATCH GIVEN AS INPUT TO EACH HIDDEN LAYER
+                Return:
+                Returns the transformed batch with mean 0 and stdev 1  
+            """          
+            means = tf.reduce_mean(batch, 0)
+            stdev = tf.reduce_mean(np.power((batch - means), 2), 0) + 0.00001
+            return (batch - means) / (tf.sqrt(stdev))
+
       @property
       def running_ops(self):
+            """ RETURNS THE TENSORS THAT NEED TO BE COMPUTED. DEPENDENCIES WILL MAKE ALL THE OTHER NECESSARY TENSORS BE COMPUTED TOO.
+            """
             if self.is_training:
                   return {
                         "accuracy": self.accuracy,
@@ -81,6 +105,9 @@ class BitDefender_Challanger(object):
                   }
 
       def run_model(self, session, writer, merged_summaries):
+            """ RUNNING MODEL ON CURRENT CONFIGURATION 
+                This method is computing training, validation or testing depending on what model is calling it.
+            """
             print("\n %s just started !" % self.model_op_name)
             accuracy = 0.0
             for batch in range(self.batch_nr):
@@ -98,35 +125,40 @@ class BitDefender_Challanger(object):
             print(session.run(self.inputs)[0][0:10])        
 
 class NormalConfig(object):
-      data_file = data_file
-      batch_size = 64
+      feature_extraction_method = 'Autoencoder'
+      data_file = data_file_train
+      test_file = data_file_test
+      batch_size = 200
       validation_split = 0.3
-      test_split = 0.3
-      variance_threshold = 0.95
+      variance_threshold = 0.96
 
 #%%
 def main():
       ses = tf.Session()
-
+      # Compute train, validation and testing data
       btc_dp = BitDefender_Data_Producer(NormalConfig())
       btc_dp.import_data(ses)
       train_inputs, train_target, train_batch_nr = btc_dp.train_data
       validation_inputs, validation_targets, validation_batch_nr = btc_dp.validation_data
-      test_inputs, test_target, test_batch_nr = btc_dp.test_data
+      test_inputs, test_targets, test_batch_nr = btc_dp.test_data
 
-      btc_train_model      = BitDefender_Challanger(train_inputs, train_target, train_batch_nr, True, 0.003, model_op_name="Training")
+      # Create the three model objects with the needed inputs
+      btc_train_model      = BitDefender_Challanger(train_inputs, train_target, train_batch_nr, True, 0.001, model_op_name="Training")
       btc_validation_model = BitDefender_Challanger(validation_inputs, validation_targets, validation_batch_nr, model_op_name="Validation")
-      btc_test_model       = BitDefender_Challanger(test_inputs, test_target, test_batch_nr, model_op_name="Testing")
+      btc_test_model       = BitDefender_Challanger(test_inputs, test_targets, test_batch_nr, model_op_name="Testing")
 
+      # Create model graphs for the 3 ops
       btc_train_model.model()
       btc_validation_model.model()
       btc_test_model.model()
 
+      # Create writer and summaries for tensorboard
       writer = tf.summary.FileWriter('./graphs', ses.graph)
       merged_summaries = tf.summary.merge_all()
 
+      # Initialize model variables and run the models. Only training model need initialization because the rest use the traind weights and biases.
       btc_train_model.initialize_variables(ses)
-      epochs = 2
+      epochs = 5
       for epoch in range(epochs):
             print("\n-----------> Epoch %d" % epoch)
             btc_train_model.run_model(ses,writer, merged_summaries)
@@ -139,7 +171,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
 
--- reader.py
+++ reader.py
@@ -1,65 +1,105 @@
 # %% Change working directory from the workspace root to the ipynb file location.
 import os
-# try:
-#     os.chdir(os.path.join(
-#         os.getcwd(), 'challange'))
-#     print(os.getcwd())
-# except:
-#     pass
-# data_file = 'challenge_train.csv'
-
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 # %%
 class Reader(object):
-    def __init__(self, data_file, batch_size, validation_split, test_split, variance_threshold):
+    def __init__(self, feature_extraction_method, data_file, test_file, batch_size, validation_split, variance_threshold):
         self._data_file = data_file
+        self._test_file = test_file
         self._batch_size = batch_size
         self.validation_split = validation_split
-        self.test_split = test_split
         self.variance_threshold = variance_threshold
+        self.feature_extraction_method = feature_extraction_method
 
     def import_data(self):
+        """ READS DATA FROM INPUT FILES AND SEPPARATES FEATURES FROM TARGETS
+            Also feature named 'md5' gets dropped because it has
+            too large values to be computed.
+
+            Class data members: 
+            self.inputs, self.targets - resulting training inputs and targets
+            self.test_inputs, self.test_targets - resulting testing inputs and targets 
+        """
         print("---------- Importing data from file")
         df = pd.read_csv(self._data_file, low_memory=False)
-        df = df.reindex(np.random.permutation(df.index))
+        df_test = pd.read_csv(self._test_file, low_memory=False)
 
+        df = df.reindex(np.random.permutation(df.index))
+        
         self.inputs = df.drop(labels=['verdict', 'md5'], axis='columns')
+        self.test_inputs = df_test.drop(labels=['md5'], axis='columns')
+
         self.inputs.replace(to_replace={True: 1, False: 0}, inplace=True)
-        self.inputs = np.array(self.inputs)
+        self.test_inputs.replace(to_replace={True: 1, False: 0}, inplace=True)        
 
         self.targets = df['verdict']
         self.targets.replace(to_replace={'trojan': 1, 'clean': 0}, inplace=True)
+
+        # self.test_targets = df_test['verdict']
+        # self.test_targets.replace(to_replace={'trojan': 1, 'clean': 0}, inplace=True)
+        self.test_targets = np.ones(self.test_inputs.shape[0]).astype(int)
+       
+
+        self.inputs = np.array(self.inputs)
+        self.test_inputs = np.array(self.test_inputs)
         self.targets = np.array(self.targets)
+        self.test_targets = np.array(self.test_targets)
 
     def _remove_zero_stdev_features(self):
+        """ DELETES FEATURES WITH ZERO STANDARD DEVIATION
+            Class data members:
+            self.stdev - vector containing standar deviations calculated on training data for each feature
+            self.mean - vector containing means calculated on training data for each feature
+            self.inputs - features used in training fase
+            self.test_inputs - testing features
+        """
         print("---------- Drop zero variance features")
         zero_variance_features = np.where(self.stdev == 0)
         self.stdev = np.delete(self.stdev, zero_variance_features)
         self.mean = np.delete(self.mean, zero_variance_features)
         self.inputs = np.delete(self.inputs, zero_variance_features, axis=1)
+        self.test_inputs = np.delete(self.test_inputs, zero_variance_features, axis=1)
 
     def scale_data(self):
+        """ SCALE DATA TO 0 MEAN AND 1 STDV
+            Class data members:
+            self.stdev - vector containing standar deviations calculated on training data for each feature
+            self.mean - vector containing means calculated on training data for each feature
+            self.inputs - features used in training fase
+            self.test_inputs - testing features
+        """
         print("---------- Scale features")
         self.mean = np.mean(self.inputs, axis=0)
         self.stdev = np.std(self.inputs, axis=0)
         self._remove_zero_stdev_features()
         self.inputs = (self.inputs - self.mean) / self.stdev
+        self.test_inputs = (self.test_inputs - self.mean) / self.stdev
 
     def split_train_validation_test(self):
-        print("---------- Split data set into train, validation and test")
-        self.train_inputs = self.inputs[0: -(int)(self.test_split * (int)(self.inputs.shape[0]) + 1)]
-        self.validation_inputs = self.train_inputs[- (int)(self.validation_split * (int)(self.train_inputs.shape[0])):]
-        self.train_inputs = self.train_inputs[0: - (int)(self.validation_split * (int)(self.train_inputs.shape[0]) + 1)]
-        self.test_inputs = self.inputs[- (int)(self.test_split * (int)(self.inputs.shape[0])):]
+        """ SPLIT INPUTS INTO TRAIN FEATURES AND VALIDATION FEATURES
+            Class data members:
+            self.train_inputs - resulting training features
+            self.validation_inputs - resulting validation features
+        """
+        print("---------- Split data set into train, validation")
+        self.train_inputs = self.inputs[0: - (int)(self.validation_split * (int)(self.inputs.shape[0]) + 1)]
+        self.validation_inputs = self.inputs[- (int)(self.validation_split * (int)(self.inputs.shape[0])):]
 
-        self.train_targets = self.targets[0: - (int)(self.test_split * self.targets.shape[0] + 1)]
-        self.validation_targets = self.train_targets[- (int)(self.validation_split * self.train_targets.shape[0]) : ]
-        self.train_targets = self.train_targets[0: - (int)(self.validation_split * self.train_targets.shape[0] + 1)]
-        self.test_targets = self.targets[- (int)(self.test_split * self.targets.shape[0]) : ]
+        self.validation_targets = self.targets[- (int)(self.validation_split * self.targets.shape[0]) : ]
+        self.train_targets = self.targets[0: - (int)(self.validation_split * self.targets.shape[0] + 1)]
 
     def pca_fit(self, data, variance_threshold, session):
+        """ CALCULATE SVD MATRICIES AND OBTAIN SINGULAR VALUES AND THE V MATRIX
+            Args:
+            data - data matrix on which SVD decomposition is calculated
+            variance_threshold - 
+            session - tensorflow session for used to compute the matricies and not their tensors 
+            Class data members:
+            self.dimensions = vector representing the eigen values with a variance larger then the requested threshold
+            self.v_matrix = the V matrix representing the eigen vectors of A^T * A
+        """
         print("---------- PCA fitting")
         singular_values, _, v_matrix = session.run(tf.svd(data))
 
@@ -69,19 +109,74 @@ class Reader(object):
 
         singular_values = np.cumsum(singular_values)
         self.dimensions = np.where(singular_values > variance_threshold)[0][0]
-        print("VARIANCE = " + str(singular_values[self.dimensions]))
-        self.v_matrix = v_matrix[:, :self.dimensions]
+        self.v_matrix = v_matrix[:, 0:self.dimensions]
 
-    def pca_transform(self, data):
+    def pca_transform(self, data, session):
+        """ TRANSFORM MAP DATA MATRIX TO THE NEW FEATURES SPACE CALCULATED IN pca_fit (usually having smaller dimension)
+            Args:
+            data - data matrix to be transformed
+            Return:
+            The new transformed matrix
+        """
         return tf.matmul(data, self.v_matrix)
 
+    def autoencoder_model(self, hidden_layer_1_dimension, hidden_layer_2_dimension):
+        """ CREATE AUTOENCODER MODEL TO IMITATE THE FUNCTIONALITY OF PCA
+            Args:
+            hidden_layer_1_dimension - dimension for first hidden layer
+            hidden_layer_2_dimension - dimension for second hidden layer
+        """
+        self.autoencoder_input = tf.placeholder(tf.float64, [None, self.train_inputs.shape[1]])
+        encoder_layer_1 = tf.layers.dense(self.autoencoder_input , int(self.autoencoder_input.shape[1]), activation=tf.nn.elu, name="First_Encoder_Layer" )
+        encoder_layer_2 = tf.layers.dense(encoder_layer_1, hidden_layer_1_dimension, activation=tf.nn.elu, name="Second_Encoder_Layer")
+        self.encoder_output = tf.layers.dense(encoder_layer_2, hidden_layer_2_dimension, activation=tf.nn.elu, name="Output_Encoder_Layer" )
+
+        decoder_layer_1 = tf.layers.dense(self.encoder_output, hidden_layer_1_dimension, activation=tf.nn.elu, name="First_Decoder_Layer")
+        self.decoder_output = tf.layers.dense(decoder_layer_1, self.autoencoder_input.shape[1], activation=tf.nn.sigmoid, name="Output_Decoder_Layer" )
+
+        reconstruction_loss = tf.reduce_mean(tf.square(self.decoder_output - self.autoencoder_input))
+        self.autoencoder_optimizer = tf.train.AdamOptimizer(0.003).minimize(reconstruction_loss)
+
+    def autoencoder_fit(self, epochs, session):
+        """ FIT THE AUTOENCODER MODEL TO THE TRAINING INPUTS
+            Args :
+            epochs - number of epochs used for training the autoencoder
+        """
+        print("---------- Autoencoder fitting")
+        session.run(tf.global_variables_initializer())
+        for _ in range(epochs):
+            session.run(self.autoencoder_optimizer, feed_dict = {self.autoencoder_input: self.train_inputs})
+
+    def autoencoder_transform(self, data, session):
+        """ TRANSFORM DATA INPUT REDUCING DIMENSIONALITY BY CALCULATING THE OUTPUT OF ONY THE ENCODER PART OF THE AUTOENCODER
+            Args :
+            data - data to be transformed
+        """
+        return session.run(self.encoder_output, feed_dict={self.autoencoder_input: data})
+
     def compute_batches(self, data, batch_nr, batch_size, target):
+        """ REORGANIZE DATA ARGUMENT IN BATCHES
+        Args :
+            data - data to be transformed
+            batch_nr - new number of raws representing the number of batches
+            batch_size - number of sambles per batch
+            target - boolean value representing if the data is a matrix of features or targets
+        """
         if not target:
             return tf.cast(tf.reshape(data[0: (int)(batch_nr * batch_size)], ((batch_nr, batch_size, data.shape[1]))), tf.float64)
         else:
             return tf.cast(tf.reshape(data[0: (int)(batch_nr * batch_size)], ((batch_nr, batch_size, 1))), tf.float64)
 
     def create_queue(self, session, X, Y,  batch_nr):
+        """ CREATE TENSORFLOW QUEUES THAT WILL PROVIDE THE DATA ONE BATCH AT A TIME
+            Args :
+            X - features to extract batch from
+            Y - targets to extract batch from
+            batch_nr - number of batches
+            Return:
+            X[i], Y[i] - features and targets batch
+            qr - queue runner used used for parallelism
+        """
         with tf.name_scope("Input_Queues"):
             queue = tf.FIFOQueue(capacity=batch_nr / 2, dtypes=[tf.int32])
             enqueue_op = queue.enqueue_many([[j for j in range(batch_nr)]])
@@ -91,34 +186,52 @@ class Reader(object):
         return X[i], Y[i], qr
 
     def batch_producer(self, session, name=None):
+        """
+            This method calls all the above defined methods in order to compute 
+            the necessary features and targets for training, validaiton and 
+            testing. 
+            User is able to use PCA or Autoencoders based on the choosed config
+            from the Challange model.
+        """
         print("---------- Produce data batches")
         self.import_data()
         self.scale_data()
         self.split_train_validation_test()
 
-        self.pca_fit(self.train_inputs, self.variance_threshold, session)
-        print("---------- PCA transformation")
-        self.train_inputs = self.pca_transform(self.train_inputs)
-        self.validation_inputs = self.pca_transform(self.validation_inputs)
-        self.test_inputs = self.pca_transform(self.test_inputs)
+        if self.feature_extraction_method == 'PCA':
+            self.pca_fit(self.train_inputs, self.variance_threshold, session)
+            print("---------- PCA transformation")
+            self.train_inputs = self.pca_transform(self.train_inputs, session)
+            self.validation_inputs = self.pca_transform(self.validation_inputs, session)
+            self.test_inputs = self.pca_transform(self.test_inputs, session)
+        else:
+            self.autoencoder_model(195,160)
+            self.autoencoder_fit(5, session)
+            self.train_inputs = self.autoencoder_transform(self.train_inputs, session)
+            self.validation_inputs = self.autoencoder_transform(self.validation_inputs, session)
+            self.test_inputs = self.autoencoder_transform(self.test_inputs, session)
+
+        self.train_inputs = tf.convert_to_tensor(self.train_inputs)
+        self.validation_inputs = tf.convert_to_tensor(self.validation_inputs)
+        self.test_inputs = tf.convert_to_tensor(self.test_inputs)
 
         train_batch_nr = (int)(self.train_inputs.shape[0]) // self._batch_size
         validation_batch_nr = 10
         validation_batch_size = self.validation_inputs.shape[0] // validation_batch_nr
 
-        self.train_inputs = (self.compute_batches(tf.convert_to_tensor(self.train_inputs),
+        self.train_inputs = (self.compute_batches((self.train_inputs),
                                                   train_batch_nr,
                                                   self._batch_size,
                                                   False))
-        self.train_targets = (self.compute_batches(tf.convert_to_tensor(self.train_targets),
+        self.train_targets = (self.compute_batches((self.train_targets),
                                                    train_batch_nr,
                                                    self._batch_size,
                                                    True))
-        self.validation_inputs = (self.compute_batches(tf.convert_to_tensor(self.validation_inputs),
+        self.validation_inputs = (self.compute_batches((self.validation_inputs),
                                                        validation_batch_nr,
                                                        validation_batch_size,
                                                        False))
-        self.validation_targets = (self.compute_batches(tf.convert_to_tensor(self.validation_targets),
+        self.validation_targets = (self.compute_batches((self.validation_targets),
                                                         validation_batch_nr,
                                                         validation_batch_size,
                                                         True))
@@ -132,7 +245,7 @@ class Reader(object):
         with tf.name_scope("BitDefender_Inputs_Test_Generator"):
             self.test_inputs =  tf.cast(tf.convert_to_tensor(self.test_inputs), tf.float64)
             self.test_targets = tf.cast(tf.reshape(tf.convert_to_tensor(self.test_targets), (self.test_targets.shape[0],1)), tf.float64)
-            X_test, y_test = (self.test_inputs, self.test_targets)
+            (X_test, y_test) = self.test_inputs, self.test_targets
 
         self._coord = tf.train.Coordinator()
         self._threads_train = qr_train.create_threads(session, self._coord, start=True)
@@ -141,40 +254,13 @@ class Reader(object):
         return (X_train, y_train), (X_validation, y_validation), (X_test, y_test), (train_batch_nr, validation_batch_nr, 1)
 
     def free_threads(self):
+        """ FREESES THE THREADS CREATED BY THE QUEUE RUNNER
+        """
         self._coord.request_stop()
         self._coord.join(self._threads_train)
         self._coord.join(self._threads_validation)
 
-# # %% This part is used for debugging only
-# with tf.Session() as ses:
-#     ses.run(tf.global_variables_initializer())
-#     rd = Reader(data_file=data_file, batch_size=20, validation_split=0.2, test_split=0.2, variance_threshold=0.95)
-#     (train_in, train_out), (valid_in, valid_out), (test_in, test_out) , (_,_,_)= rd.batch_producer(ses)
-#     print(ses.run(train_in).shape)
-#     print(ses.run(train_out).shape)
-#     print(ses.run(train_in)[0][0:10])
-#     print(ses.run(test_out)[0])
-#     print(ses.run(train_in)[0][0:10])
-#     print(ses.run(test_out)[0])
-
-#     print(ses.run(valid_in).shape)
-#     print(ses.run(valid_out).shape)
-#     print(ses.run(valid_in)[0][0:10])
-#     print(ses.run(valid_out)[0])
-#     print(ses.run(valid_in)[0][0:10])
-#     print(ses.run(valid_out)[0])
-
-#     print(ses.run(test_in).shape)
-#     print(ses.run(test_out).shape)
-#     print(ses.run(test_in)[0][0:10])
-#     print(ses.run(test_out)[0])
-#     print(ses.run(test_in)[0][0:10])
-#     print(ses.run(test_out)[0])
-
-#     rd.free_threads()
-
-
-# #%%
+#%%
 
 
 #%%
